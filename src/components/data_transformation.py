c0f83e49ac4dee94ac7ba9503a243f51eb41d6b4
Xing Ping <112884750+Shimul-Zahan@users.noreply.github.com>
2025-02-08 01:47:08 +0600

    ☠️☠️Data preprocessing working

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -14,5 +14,13 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer       # for the missing value
 from sklearn.pipeline import Pipeline          # for create the pipeline
 
+'''
+categorical_columns = ['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch', 'test_preparation_course']
+numerical_columns = ['math_score', 'reading_score', 'writing_score']
+
+'''
+
 class DataTransformationConfig:
-    pass          
+    preprocessor
+
+         
