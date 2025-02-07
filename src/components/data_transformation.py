03d8b42443d51f42ec4fb0d1280e6ad84debc7af
Xing Ping <112884750+Shimul-Zahan@users.noreply.github.com>
2025-02-08 01:41:36 +0600

    ☠️☠️End for today

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,18 @@
 # All the transformation code here
+# categorical to numerical
+import sys
+import os
+from dataclasses import dataclass
+
+from src.logger import logging
+from src.exception import CustomException
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer  #  for complete all the task at once
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer       # for the missing value
+from sklearn.pipeline import Pipeline          # for create the pipeline
+
+class DataTransformationConfig:
+    pass          
